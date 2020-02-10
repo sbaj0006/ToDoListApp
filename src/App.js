de735b741e8e8223678b6fbab49b08dc1c7589e0
Smarth Bajaj <smarthbajaj28@gmail.com>
2020-02-10 18:37:04 +1100

    App component structure

--- src/App.js
+++ src/App.js
@@ -5,14 +5,61 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/ToDoInput";
 import TodoList from "./components/TodoList";
 
+//added bootstrap
 export default class App extends Component {
+  state = {
+    items: [
+      {
+        id: 1,
+        title: "wake up"
+      },
+      {
+        id: 2,
+        title: "make breakfast"
+      }
+    ],
+    id: uuid(),
+    item: "",
+    editItem: false
+  };
+
+  handleChange = e => {
+    console.log("handle change");
+  };
+
+  handleSubmit = e => {
+    console.log("handle submit");
+  };
+
+  clearList = () => {
+    console.log("clear list");
+  };
+  handleDelete = id => {
+    console.log(`handle edit ${id}`);
+  };
+  handleEdit = id => {
+    console.log(`edit edit ${id}`);
+  };
   render() {
     return (
       <div>
         <div className="container">
           <div className="row">
-            <TodoInput />
-            <TodoList />
+            <div className="col-10 mx-auto col-md-8 mt-5">
+              <h3 className="text-capitalize text-center">To do input</h3>
+              <TodoInput
+                item={this.state.item}
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                editItem={this.state.editItem}
+              />
+              <TodoList
+                items={this.state.items}
+                clearList={this.clearList}
+                handleDelete={this.handleDelete}
+                handleEdit={this.handleEdit}
+              />
+            </div>
           </div>
         </div>
       </div>
